df61d227e2993c01e5db416500142ac48da80fc0
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-01-19 05:09:24 -0300

    Aula 03.2

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,25 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  return (
+    <div
+      role="progressbar"
+      aria-label="Progresso de hábitos completados nesse dia."
+      aria-valuenow={props.progress}
+      className="h-3 rounded-xl bg-violet-600"
+      style={{ width: `${props.progress}%` }}
+    />
+  )
+}
+
+{/* 
+import * as Progress from '@radix-ui/react-progress';
+
+  <Progress.Root className="relative overflow-hidden bg-zinc-700 rounded-xl w-[300px] h-[12px] mt-4" value={65}>
+    <Progress.Indicator
+      className="bg-violet-600 w-full h-full"
+      style={{ transform: `translateX(-${100 - 90}%)` }}
+    />
+  </Progress.Root> */}
