1abb1ec0669c297c3a36763adce68a957d344436
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-01-22 17:03:07 -0300

    Aula 05.2

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -55,7 +55,7 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="ex.: Exercícios, dormir 8h, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
         onChange={event => setTitle(event.target.value)}
@@ -70,17 +70,17 @@ export function NewHabitForm() {
           return (
             <Checkbox.Root
               key={weekDay}
-              className='flex items-center gap-3 group'
+              className='flex items-center gap-3 group focus:outline-none'
               checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                 <Checkbox.Indicator>
                   <Check size={20} className='text-white' />
                 </Checkbox.Indicator>
               </div>
 
-              <span className='text-white leading-tight'>
+              <span className='text-white leading-tight focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900'>
                 {weekDay}
               </span>
             </Checkbox.Root>
@@ -90,7 +90,9 @@ export function NewHabitForm() {
 
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 transition hover:bg-green-500">
+      <button
+      type="submit"
+      className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 transition-colors hover:bg-green-500">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
